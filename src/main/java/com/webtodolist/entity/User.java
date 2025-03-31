ebc1057c362b840a044bff5f59d684be395b828e
BoomTrading <orytami@gmail.com>
2025-03-31 22:25:22 +0200

    Aggiunto Project + User

---
+++ src/main/java/com/webtodolist/entity/User.java
@@ -0,0 +1,61 @@
+package com.webtodolist.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String username;
+
+    @Column(nullable = false, length = 45)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Column(length = 45)
+    private String nome;
+
+    @Column(length = 45)
+    private String cognome;
+
+    @OneToMany(mappedBy = "user")
+    private List<Project> projects;
+
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks;
+
+    @OneToMany(mappedBy = "user")
+    private List<Comment> comments;
+
+    // Enum for user roles
+    public enum UserRole {
+        ADMIN, USER, MANAGER
+    }
+}
