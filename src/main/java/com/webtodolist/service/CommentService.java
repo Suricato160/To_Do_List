f7014f7a85618a8583e5472cbbec523daac57e5e
Suricato160 <pascoloadriano@gmail.com>
2025-04-01 09:35:59 +0200

    sistemato

--- src/main/java/com/webtodolist/service/CommentService.java
+++ src/main/java/com/webtodolist/service/CommentService.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CommentService {
-<<<<<<< Updated upstream
 
     Optional<Comment> findById(int id);
     
@@ -22,20 +21,4 @@ public interface CommentService {
     
     List<Comment> findAllComments();
 }
-=======
-    
-    List<Comment> findByIdTask(int taskId);
-    
-    List<Comment> findByIdUser(int userId);
-    
-    Comment saveComment(Comment comment);
-    
-    void deleteComment(int id);
-    
-    void deleteByIdTask(int taskId);
-    
-    List<Comment> findAllComments();
-    
-    Optional<Comment> findById(int id);
-}
->>>>>>> Stashed changes
+
--- src/main/java/com/webtodolist/service/CommentServiceImpl.java
+++ src/main/java/com/webtodolist/service/CommentServiceImpl.java
@@ -2,10 +2,7 @@ package com.webtodolist.service;
 
 import com.webtodolist.entity.Comment;
 import com.webtodolist.repository.CommentRepository;
-<<<<<<< Updated upstream
-=======
 import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> Stashed changes
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,37 +13,24 @@ public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
 
-<<<<<<< Updated upstream
-=======
     @Autowired
->>>>>>> Stashed changes
     public CommentServiceImpl(CommentRepository commentRepository) {
         this.commentRepository = commentRepository;
     }
 
     @Override
-<<<<<<< Updated upstream
     public Optional<Comment> findById(int id) {
         return commentRepository.findById(id);
     }
 
     @Override
     public List<Comment> findByTaskId(int taskId) {
-        return commentRepository.findByTaskId(taskId);
+        return commentRepository.findByTaskId(taskId); // Allineato al repository
     }
 
     @Override
     public List<Comment> findByUserId(int userId) {
-        return commentRepository.findByUserId(userId);
-=======
-    public List<Comment> findByIdTask(int taskId) {
-        return commentRepository.findByIdTask(taskId);
-    }
-
-    @Override
-    public List<Comment> findByIdUser(int userId) {
-        return commentRepository.findByIdUser(userId);
->>>>>>> Stashed changes
+        return commentRepository.findByUserId(userId); // Allineato al repository
     }
 
     @Override
@@ -55,35 +39,17 @@ public class CommentServiceImpl implements CommentService {
     }
 
     @Override
-<<<<<<< Updated upstream
     public void deleteById(int id) {
-=======
-    public void deleteComment(int id) {
->>>>>>> Stashed changes
         commentRepository.deleteById(id);
     }
 
     @Override
-<<<<<<< Updated upstream
     public void deleteByTaskId(int taskId) {
-        commentRepository.deleteByTaskId(taskId);
-=======
-    public void deleteByIdTask(int taskId) {
-        commentRepository.deleteByIdTask(taskId);
->>>>>>> Stashed changes
+        commentRepository.deleteByTaskId(taskId); // Allineato al repository
     }
 
     @Override
     public List<Comment> findAllComments() {
         return commentRepository.findAll();
     }
-<<<<<<< Updated upstream
 }
-=======
-
-    @Override
-    public Optional<Comment> findById(int id) {
-        return commentRepository.findById(id);
-    }
-}
->>>>>>> Stashed changes
